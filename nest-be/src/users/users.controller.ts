bcf4382505622a6d45cd23408abc2d78155bd909
Hong-Jun-Il <86016726+Hong-Jun-Il@users.noreply.github.com>
2024-09-08 00:56:26 +0900

    save

---
+++ nest-be/src/users/users.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get() // Get /users
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return this.usersService.findAll(role);
+  }
+
+  @Get(':id') // Get /users/:id
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(+id);
+  }
+
+  @Post()
+  create(
+    @Body()
+    user: {
+      name: string;
+      email: string;
+      role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.usersService.create(user);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body()
+    updatedUser: {
+      name?: string;
+      email?: string;
+      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.usersService.update(+id, updatedUser);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.usersService.delete(+id);
+  }
+}
---
+++ nest-be/src/users/users.service.ts
@@ -0,0 +1,93 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UsersService {
+  private users = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      email: 'Sincere@april.biz',
+      role: 'INTERN',
+    },
+    {
+      id: 2,
+      name: 'Ervin Howell',
+      email: 'Shanna@melissa.tv',
+      role: 'INTERN',
+    },
+    {
+      id: 3,
+      name: 'Clementine Bauch',
+      email: 'Nathan@yesenia.net',
+      role: 'ENGINEER',
+    },
+    {
+      id: 4,
+      name: 'Patricia Lebsack',
+      email: 'Julianne.OConner@kory.org',
+      role: 'ENGINEER',
+    },
+    {
+      id: 5,
+      name: 'Chelsey Dietrich',
+      email: 'Lucio_Hettinger@annie.ca',
+      role: 'ADMIN',
+    },
+  ];
+
+  findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    if (role) {
+      return this.users.filter((user) => user.role === role);
+    }
+
+    return this.users;
+  }
+
+  findOne(id: number) {
+    const user = this.users.find((user) => user.id === id);
+
+    return user;
+  }
+
+  create(user: {
+    name: string;
+    email: string;
+    role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+  }) {
+    const usersByHighestId =
+      [...this.users].sort((a, b) => b.id - a.id)[0].id + 1;
+
+    const newUser = {
+      id: usersByHighestId,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    };
+
+    this.users.push(newUser);
+
+    return newUser;
+  }
+
+  update(
+    id: number,
+    updatedUser: {
+      name?: string;
+      email?: string;
+      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    const targetIndex = this.users.findIndex(user=>user.id === id);
+    this.users[targetIndex] = {...this.users[targetIndex], ...updatedUser};
+
+    return this.findOne(id);
+  }
+
+  delete(id: number){
+    const targetUser = this.findOne(id);
+
+    this.users = this.users.filter((user)=>user.id !== targetUser.id);
+
+    return targetUser;
+  }
+}
